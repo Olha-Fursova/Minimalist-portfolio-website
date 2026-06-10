8ca91a58eb2f686ee0d1ae60ee4d223897fe5032
Olha Fursova <teor15manypeople1@gmail.com>
2026-06-10 19:19:37 +0200

    Add pages components, icons sprite, fix router config in main.jsx, change in basic AppBar, add colors to variables

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import AppBar from "./AppBar/AppBar.jsx";
-import MobileMenu from "./MobileMenu/MobileMenu.jsx";
-import Loader from "./Loader/Loader.jsx";
-import Footer from "./Footer/Footer.jsx";
+import AppBar from "../AppBar/AppBar.jsx";
+import MobileMenu from "../MobileMenu/MobileMenu.jsx";
+import Loader from "../Loader/Loader.jsx";
+import Footer from "../Footer/Footer.jsx";
 import { lazy, Suspense, useState } from "react";
 
 import css from "./App.module.css";
 
-const Home = lazy(() => import("../pages/Home/Home.jsx"));
+const Home = lazy(() => import("../../pages/Home/Home.jsx"));
+const Portfolio = lazy(() => import("../../pages/Portfolio/Portfolio.jsx"));
+const Contact = lazy(() => import("../../pages/Contact/Contact.jsx"));
 
 const App = () => {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
@@ -25,6 +27,8 @@ const App = () => {
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Suspense>
       </main>
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,35 +4,34 @@ import clsx from "clsx";
 import css from "./AppBar.module.css";
 
 const buildLinkClass = ({ isActive }) => {
-  return clsx(css["nav-link"], isActive && css.active);
+  return clsx(css["nav__link"], isActive && css.active);
 };
 
 const AppBar = ({ isOpen, onOpen }) => {
   return (
     <header className={css["header-container"]}>
+      <NavLink to="/" className={css.logo}>
+          <svg className={css["logo__icon"]}>
+            <use href="/icons.svg#icon-logo" />
+          </svg>
+      </NavLink>
+
       <nav className={css.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
         <button
-          className={css["modal-button"]}
+          className={css["modal__button"]}
           type="button"
           aria-expanded={isOpen}
           aria-controls="mobile-menu"
           aria-label="open menu"
           onClick={onOpen}
         >
-          <svg className={css["mobile-btn"]}>
+          <svg className={css["mobile__btn"]}>
             <use href="/img/icons.svg#icon-mobile-menu-button" />
           </svg>
         </button>
-        <NavLink to="/" className={buildLinkClass}>
-          Home
-        </NavLink>
-        {/* <NavLink to="/" className={css.logo}>
-          <img
-            className={css.logo}
-            src="/img/MokkaLogo.png"
-            alt="MokkaMokka logo cat lazy laying on top of name and next to a coffee cup"
-          />
-        </NavLink> */}
       </nav>
     </header>
   );
