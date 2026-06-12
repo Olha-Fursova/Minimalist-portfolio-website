ee2370cc0d94bf4f32f592cf670599c692420a77
Olha Fursova <teor15manypeople1@gmail.com>
2026-06-12 18:42:26 +0200

    Add About section to the Home page with mobile-first/tablet/desktop styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import Hero from "./sections/Hero/Hero";
+import About from "./sections/About/About";
 
 const Home = () => {
   return (
     <>
       <Hero />
+
+      <About />
     </>
   )
 }
---
+++ src/pages/Home/sections/About/About.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router";
+import ResponsiveImage from "../../../../components/images/ResponsiveImage/ResponsiveImage";
+
+import css from "./About.module.css";
+
+const About = () => {
+  return (
+    <section className="section">
+      <div className={`container ${css.about__container}`}>
+        <ResponsiveImage
+          baseName="image-homepage-profile"
+          alt="Portrait of the website owner with a side profile."
+          folderName="homepage"
+        />
+        <div className={css.about_content}>
+          <div className={css.deviding__line}></div>
+
+          <article className={css.about__article}>
+            <h2 className={css.about__header}>About Me</h2>
+
+            <p className={css.about__bio}>
+              I’m a junior front-end developer looking for a new role in an
+              exciting company. I focus on writing accessible HTML, using modern
+              CSS practices and writing clean JavaScript. When writing
+              JavaScript code, I mostly use React, but I can adapt to whatever
+              tools are required. I’m based in London, UK, but I’m happy working
+              remotely and have experience in remote teams. When I’m not coding,
+              you’ll find me outdoors. I love being out in nature whether that’s
+              going for a walk, run or cycling. I’d love you to check out my
+              work.
+            </p>
+
+            <Link to="/portfolio" className="btn btn__secondary">
+              Go to portfolio
+            </Link>
+          </article>
+
+          <div className={css.deviding__line}></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
