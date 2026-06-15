6fb34290bdbd1825f03e189fd94685dd8c366350
Olha Fursova <teor15manypeople1@gmail.com>
2026-06-15 23:57:37 +0200

    Projects data

---
+++ src/data/projects-preview.js
@@ -0,0 +1,30 @@
+export const projectsPreviews = [
+  {
+    id: "1",
+    name: "Manage",
+    image__name: "image-portfolio-manage",
+    image__alt: "Landing Page of Manage app in pink color.",
+    description: "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider."
+  },
+  {
+    id: "2",
+    name: "Bookmark",
+    image__name: "image-portfolio-bookmark",
+    image__alt: "Landing Page of Bookmark app in blue color.",
+    description: "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section."
+  },
+  {
+    id: "3",
+    name: "Insure",
+    image__name: "image-portfolio-insure",
+    image__alt: "Landing Page of Insure app in dark-purple color.",
+    description: "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation."
+  },
+  {
+    id: "4",
+    name: "Fylo",
+    image__name: "image-portfolio-fylo",
+    image__alt: "Landing Page of Fylo app in teal color.",
+    description: "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes."
+  }
+]
