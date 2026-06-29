545d79b2a153c950c0041f13ebd03e3a6efa3bdf
Olha Fursova <teor15manypeople1@gmail.com>
2026-06-29 16:46:23 +0200

    Replace BrowserRouter import source

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/main.css";
 import App from "./components/App/App.jsx";
 
